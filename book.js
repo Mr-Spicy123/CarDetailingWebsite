ea6edc793512d316664d3105696f10567c72cacf
Mr-Spicy123 <jspic1@ocdsb.ca>
2025-06-13 16:29:43 -0300

    Update

---
+++ book.js
@@ -0,0 +1,14 @@
+var message;
+
+function interior() {
+    window.location.href="confirmation.html";
+    message="2 hour job+$80";
+}
+function exterior() {
+    window.location.href="confirmation.html";
+    message="2 hour job+$100";
+}
+function both() {
+    window.location.href="confirmation.html";
+    message="3 hour job+$150";
+}
