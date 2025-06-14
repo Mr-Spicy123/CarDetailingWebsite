ec1f5b76549538024019d35e0158289687e40cf2
Mr-Spicy123 <jspic1@ocdsb.ca>
2025-06-13 22:29:41 -0300

    Update

--- book.js
+++ book.js
@@ -1,6 +1,8 @@
 
 
-function setJobType(jobType) {
+function setJobVars(jobType, jobPrice, jobTime) {
     localStorage.setItem("jobType", jobType);
+    localStorage.setItem("jobPrice", jobPrice);
+    localStorage.setItem("jobTime", jobTime);
     window.location.href = "confirmation.html";
 }
