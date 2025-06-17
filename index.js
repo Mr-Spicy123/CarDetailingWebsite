2ab45c4194717d6f3cbe507ad71cca0643ffe968
Mr-Spicy123 <jspic1@ocdsb.ca>
2025-06-17 15:45:07 -0300

    Update

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ function bookPage() {
     window.location.href = "book.html";
   }
 
-  const images = [
-    "BEFORE_TEMPLATE.png",
-    "AFTER_TEMPLATE.png"
+  const beforeImages = [
+    "BeforeOne.jpg",
+    "BeforeTwo.jpg",
+    "BeforeThree.jpg",
+    "BeforeFour.jpg"
   ];
 
+  const afterImages = [
+    "AfterOne.jpg",
+    "AfterTwo.jpg",
+    "AfterThree.jpg",
+    "AfterFour.jpg"
+  ]
+
   let index = 0;
-const imgElement = document.getElementById("beforeimage");
+const beforeImgElement = document.getElementById("beforeimage");
+const afterImgElement = document.getElementById("afterimage");
 
 setInterval(() => {
-  index = (index + 1) % images.length;
-  imgElement.src = images[index];
-}, 3000); // 3000ms = 3 seconds
+  index = (index + 1) % beforeImages.length;
+  beforeImgElement.src = beforeImages[index];
+  afterImgElement.src = afterImages[index];
+}, 5000); //ms
