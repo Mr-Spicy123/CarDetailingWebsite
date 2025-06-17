9c40019ae3a9f00406c0a864691916728d7a64bf
Mr-Spicy123 <jspic1@ocdsb.ca>
2025-06-17 14:30:07 -0300

    Update

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function bookPage() {
     window.location.href = "book.html";
   }
+
+  const images = [
+    "BEFORE_TEMPLATE.png",
+    "AFTER_TEMPLATE.png"
+  ];
+
+  let index = 0;
+const imgElement = document.getElementById("beforeimage");
+
+setInterval(() => {
+  index = (index + 1) % images.length;
+  imgElement.src = images[index];
+}, 3000); // 3000ms = 3 seconds
